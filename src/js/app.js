5e279e59ee721cc622408171a50accf28a73a210
siiion <siwonjeon234@gmail.com>
2024-09-28 17:47:48 +0900

    Refactor: DOMContentLoaded 삭제

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,3 @@
 import { getFormattedDate } from "./utils/dateUtils.js";
 
-// DOMContentLoaded 이벤트 발생 시 날짜를 설정
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("current-date").textContent = getFormattedDate();
-});
+document.getElementById("current-date").textContent = getFormattedDate();
