a43c10672170c9be42d35cb7715135584b23b9c5
siiion <siwonjeon234@gmail.com>
2024-09-30 18:31:10 +0900

    Feat: 뉴스 자동 롤링 영역 기본 틀

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,8 @@
 import { getFormattedDate } from "./utils/dateUtils.js";
+import { createRoller } from "./utils/rollerUtils.js";
 
 document.getElementById("current-date").textContent = getFormattedDate();
+createRoller(".news-1-contents");
+setTimeout(() => {
+  createRoller(".news-2-contents");
+}, 1000);
